d19d2ed1e26a002e45ee333cd55a366c7ebf5781
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-12-15 20:45:54 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home.js';
+import Table from './components/pages/Table/Table.js';
+import PageNotFound from './components/pages/PageNotFound/PageNotFound.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/table/:id' element={<Table />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </main>
   );
 }
 
